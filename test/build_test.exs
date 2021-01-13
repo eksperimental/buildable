fca7a1b7e98f3d4a2f163d66aa18f8c2506c01e4
Eksperimental <eksperimental@autistici.org>
2021-01-12 22:58:56 -0500

    Add tests for Build.into/2-3

--- test/build_test.exs
+++ test/build_test.exs
@@ -19,12 +19,31 @@ defmodule BuildTest do
     assert Build.insert(foo, {:d, 8}) == %{__struct__: Foo, map: %{a: 2, b: 4, c: 6, d: 8}}
   end
 
+  test "into/2" do
+    assert Build.into([], 1..5) == [1, 2, 3, 4, 5]
+    assert Build.into(%{}, a: 1, b: 2) == %{a: 1, b: 2}
+    assert Build.into(%{c: 3}, a: 1, b: 2) == %{a: 1, b: 2, c: 3}
+    assert Build.into([], %{a: 1, b: 2}) == [a: 1, b: 2]
+    assert Build.into([], 1..3) == [1, 2, 3]
+    assert Build.into("", ["H", "i"]) == "Hi"
+  end
+
   test "into/3" do
     assert Build.into(%Foo{}, [a: 1, b: 2, c: 3], fn {k, v} -> {k, v * 2} end) ==
              %{__struct__: Foo, map: %{a: 2, b: 4, c: 6}}
 
     assert Build.into(%Foo{}, [a: 1, b: 2, c: 3], fn {k, v} -> {k, v * 2} end) ==
              %{__struct__: Foo, map: %{a: 2, b: 4, c: 6}}
+
+    assert Build.into([], 1..5, fn x -> x * 2 end) == [2, 4, 6, 8, 10]
+    assert Build.into("numbers: ", 1..3, &to_string/1) == "numbers: 123"
+
+    assert Build.into([], [1, 2, 3], fn x -> x * 2 end) == [2, 4, 6]
+    assert Build.into("numbers: ", [1, 2, 3], &to_string/1) == "numbers: 123"
+
+    assert_raise FunctionClauseError, fn ->
+      Build.into(%{a: 1}, [2, 3], & &1)
+    end
   end
 
   test "to_empty", %{foo: foo} do
