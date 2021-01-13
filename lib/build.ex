82839ce43c2a847df8a6e3b99c42fd973c03a9bc
Eksperimental <eksperimental@autistici.org>
2021-01-12 22:59:54 -0500

    Clean out leftover code

--- lib/build.ex
+++ lib/build.ex
@@ -186,11 +186,6 @@ defmodule Build do
     reduce(buildable, [], reducer) |> reverse()
   end
 
-  # def map(enumerable, fun) do
-  #   reducer = fn x, acc -> {:cont, [fun.(x) | acc]} end
-  #   Enumerable.reduce(enumerable, {:cont, []}, reducer) |> elem(1) |> :lists.reverse()
-  # end
-
   #############################
   # reduce/2
 
